342ffa05356370fcc5aa7b7885e4e91a071dfaa0
Dan Berger <dsberger@gmail.com>
2015-03-19 13:08:22 -0700

    converted DB to postgres and added a bunch of secure password business.

--- foo-app/db/schema.rb
+++ foo-app/db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150318161107) do
+ActiveRecord::Schema.define(version: 20150319200233) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
--- foo-app/db/seeds.rb
+++ foo-app/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+
+10.times do |i|
+  User.create(username: "user#{i}", email: "user#{i}@email.com")
+end
