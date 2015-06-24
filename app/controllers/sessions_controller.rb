3fb0be31d5c9d62594abb4bc2ff3019c42e22acb
niconan <nick.hax@gmail.com>
2015-06-23 23:38:18 -0700

    login/logout working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,19 @@
 class SessionsController < ApplicationController
+  def create
+    @user = User.find_by_email(params[:email])
+    if @user && @user.authenticate(params[:password])
+      sign_in(@user)
+      flash[:success] = "Successfully signed you in!"
+      redirect_to users_path
+    else
+      flash[:error] = "Couldn't log you in :("
+      render :new
+    end
+  end
+
+  def destroy
+    sign_out
+    flash[:success] = "See you, space cowboy..."
+    redirect_to users_path
+  end
 end
