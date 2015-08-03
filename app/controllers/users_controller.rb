c67fa347edb1a97ccc3eae7545841fb0e65151b6
Garrett <garrettweinert@gmail.com>
2015-08-03 10:43:46 -0700

    basic auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
+  skip_filter :authenticate, only: [:show, :index]
+
   # GET /users
   # GET /users.json
   def index
