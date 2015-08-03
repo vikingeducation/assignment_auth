58baddac36428b807b6860f8526b671c97fc8e46
alokp <alokpra@gmail.com>
2015-08-03 12:13:44 -0700

    working on sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,24 @@
 class SessionsController < ApplicationController
+
+	def new
+	end
+
+	def create
+		@user = User.find_by_email(params[:email])
+		if @user && @user.authenticate(params[:password])
+			sign_in(@user)
+			flash[:success] = "Logged In"
+			redirect_to users
+		else
+			flash.now[:error] = "Could not sign in"
+			render :new
+		end
+	end
+
+	def destroy
+		sign_out
+		flash[:success] = "Signed out"
+		redirect_to new_user_path
+	end
+
 end
