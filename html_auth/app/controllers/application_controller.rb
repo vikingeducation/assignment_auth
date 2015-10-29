1c192029d73a19de3802334c1d4e253387f25a54
yushch <yschen9218@gmail.com>
2015-10-29 23:00:46 +0000

    change authentication process to digest authentication

--- html_auth/app/controllers/application_controller.rb
+++ html_auth/app/controllers/application_controller.rb
@@ -2,7 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+=begin
   http_basic_authenticate_with  :name => "foo", 
                               :password => "bar",
                               :only => [:update, :create]
+=end
+  before_action :authenticate_user
+  
+  USERS = { "foo" => "bar" }
+  
+  private
+  def authenticate_user
+    authenticate_or_request_with_http_digest do |username|
+      USERS[username]
+    end
+  end
 end
