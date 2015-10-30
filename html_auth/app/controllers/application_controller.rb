630db58376ea2694d4a09b798741640eb34c0f1c
yushch <yschen9218@gmail.com>
2015-10-30 09:33:17 +0000

    session based assignment finished

--- html_auth/app/controllers/application_controller.rb
+++ html_auth/app/controllers/application_controller.rb
@@ -2,11 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-=begin
+  
+  before_action :require_login
+=begin this comment was for basic authentications
   http_basic_authenticate_with  :name => "foo", 
                               :password => "bar",
                               :only => [:update, :create]
 =end
+=begin this comment was for digest authentication
   before_action :authenticate_user
   
   USERS = { "foo" => "bar" }
@@ -17,4 +20,49 @@ class ApplicationController < ActionController::Base
       USERS[username]
     end
   end
+=end
+
+
+  private
+  # Simply store our ID in the session
+  # and set the current user instance var
+  def sign_in(user)
+    session[:user_id] = user.id
+    @current_user = user
+  end
+
+  # reverse the sign in...
+  def sign_out
+    @current_user = nil
+    session.delete(:user_id)
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
+
+  # Will turn the current_user into a boolean
+  # e.g. `nil` becomes false and anything else true.
+  def signed_in_user?
+    !!current_user
+  end
+  helper_method :signed_in_user?
+  
+  def require_login
+    unless signed_in_user?
+      flash[:error] = "You are not supposed to do this without signed in"
+      redirect_to login_path
+    end
+  end
+
+  def require_current_user
+
+    unless params[:id] == current_user.id.to_s
+      flash[:error] = "You're not authorized to view this."
+      redirect_to root_url
+    end
+  end
+
 end
--- html_auth/app/controllers/users_controller.rb
+++ html_auth/app/controllers/users_controller.rb
@@ -1,17 +1,24 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  #before_action :set_user, only: [:show, :edit, :update, :destroy]
   
-  skip_filter :authenticate_user, :only => [:show, :index]
+  #skip_filter :authenticate_user, :only => [:show, :index]
+  skip_before_action :require_login, :only => [:new, :create, :index, :show]
+  
+  before_action :require_current_user, :only => [:edit, :update, :destroy]
+  
+  #skip_before_action :require_login, :only => [:new, :create, :index, :show]
 
   # GET /users
   # GET /users.json
   def index
+    #reset_session
     @users = User.all
   end
 
   # GET /users/1
   # GET /users/1.json
   def show
+    @user = User.find(params[:id])
   end
 
   # GET /users/new
@@ -21,46 +28,59 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
+    @user = User.find(params[:id])
   end
 
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
+    @user = User.new(whitelisted_user_params)
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        
+        sign_in(@user)
+        flash[:success] = "new user created"
+        redirect_to user_path(@user)
+        #format.html { redirect_to @user, notice: 'User was successfully created.' }
+        #format.json { render :show, status: :created, location: @user }
       else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        flash.now[:error] = "fail to create one"
+        render :new
+        #format.html { render :new }
+        #format.json { render json: @user.errors, status: :unprocessable_entity }
       end
-    end
   end
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
+    #respond_to do |format|
+    @user = User.find(params[:id])
+      if @user.update(whitelisted_user_params)
+        flash[:success] = "user updated"
+        #format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        #format.json { render :show, status: :ok, location: @user }
+        redirect_to users_path
       else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        flash.now[:error] = "user update fail"
+        render :edit
       end
-    end
+    #end
   end
 
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
+    @user = User.find(params[:id])
     @user.destroy
+    reset_session
+    flash[:success] = "user has been deleted and logged out"
+=begin
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
     end
+=end
+    redirect_to users_path
   end
 
   private
@@ -72,5 +92,14 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:username, :email)
+    end
+    
+    def whitelisted_user_params
+      params.require(:user).permit(
+                                :username,
+                                :email,
+                                :password,
+                                :password_confirmation
+                                )
     end
 end
