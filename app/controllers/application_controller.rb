60fc74c0ac6c50db2276b6e9a204ef17eff7be48
Julia Herron <julia.herron@gmail.com>
2016-02-11 09:24:50 -0800

    simple digest auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,23 @@
 class ApplicationController < ActionController::Base
+
+  USERS = {"nora" => "password"}
+
+  before_action :authenticate
+
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  http_basic_authenticate_with  name: "foo",
-                                password: "bar",
-                                except: [:index, :show]
+  # http_basic_authenticate_with  name: "foo",
+  #                               password: "bar",
+  #                               except: [:index, :show]
+
+  def authenticate
+    authenticate_or_request_with_http_digest do |username|
+      USERS[username]
+    end
+  end
+
+
 end
