185866ba4190a52ee5211c793d6ee42edf16db77
koziscool <kozjob@yahoo.com>
2016-02-11 11:09:44 -0600

    moved auth to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
+
+  http_basic_authenticate_with name: "foo", 
+          :password => "bar",
+          :except => [ :index, :show ]
+
+
   # GET /users
   # GET /users.json
   def index
