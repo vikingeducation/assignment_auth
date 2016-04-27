c200bae0e080ce6444b31c6278cf43cdb68bbe45
Steven Chang <prime_pork@hotmail.com>
2016-04-27 16:41:43 +1000

    created a seed file which destroys all current users and creates a defined number of users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+MULTIPLIER = 10
+
+puts "Destroying All Users"
+User.destroy_all
+
+puts "Creating #{MULTIPLIER} #{'user'.pluralize(MULTIPLIER)}"
+MULTIPLIER.times do |x|
+  User.create(:username => "user_#{x}",
+              :email    => "user_#{x}@hotmail.com"
+              )
+end
+
+puts "DONE"
