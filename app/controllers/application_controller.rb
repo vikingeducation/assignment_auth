fc94129efeecd554018e7728bac5ed3f906a573d
Steven Chang <prime_pork@hotmail.com>
2016-04-27 16:22:31 +1000

    changed to HTTP_Basic_Digest edition

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  http_basic_authenticate_with  :name     => "foo",
-                                :password => "bar",
-                                :except   => [:index, :show]
+  USERS = { "foo" => "bar" }
+
+  before_action :authenticate
+
+  def authenticate
+    authenticate_or_request_with_http_digest do |username|
+      USERS[username]
+    end
+  end
 end
