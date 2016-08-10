932b3e66fa74068a96ec8d779c13082d0d134fac
Morgan Martin <morganm5201@gmail.com>
2016-08-10 12:37:30 -0400

    implement http_digest

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  USERS = { 'morgan' => 'password'}
+  before_action :authenticate
+
+
+
+
+
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_digest do |username|
+      USERS[username]
+    end
+  end
 end
