27c001b98bf8ac04e909ddace1ec0ee9f6045e01
Hannah Squier <hannahsquier@gmail.com>
2016-08-10 09:52:01 -0700

    create seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Users.destroy_all
+
+10.times do
+  User.create(email: Faker::Internet.email, username:Faker::Internet.user_name )
+end
