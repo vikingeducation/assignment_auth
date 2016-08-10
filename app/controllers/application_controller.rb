be392d76ceeabd117f8b2280dc5389980944b5cb
Mike Lee <asackofwheat@gmail.com>
2016-08-10 12:24:57 -0400

    basic auth

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,8 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  http_basic_authenticate_with :name => "foo",
+                               :password => "bar",
+                               :except => [:index, :show]
+  
+end
