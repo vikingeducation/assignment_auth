ce44684f9585474f173e3b51745a0ce80826c8de
Leo Ahnn <leoahnn@gmail.com>
2016-08-10 12:51:14 -0400

    finished digest auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  http_basic_authenticate_with  name: "Leo",
-                                password: "password",
-                                only: [:update, :edit, :destroy]
+  # http_basic_authenticate_with  name: "Leo",
+  #                               password: "password",
+  #                               only: [:update, :edit, :destroy]
+
+  before_action :authenticate, only: [:update, :edit, :destroy]
+  USERS = { "Leo" => "password"}
+
+
+  def authenticate
+    authenticate_or_request_with_http_digest do |username|
+      USERS[username]
+    end
+  end
 end
