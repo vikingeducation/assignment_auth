55e0c5a2345303b91a05b2c9475ec4b091f9904e
BranLiang <lby89757@hotmail.com>
2016-09-02 15:25:59 +0800

    finish the http basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  http_basic_authenticate_with :name => 'foo',
+                               :password => 'bar',
+                               :except => [:index, :show]
 end
