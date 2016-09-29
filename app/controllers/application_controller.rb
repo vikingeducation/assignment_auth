c1e216ce3aa603cfb2e87c1a7d5570d3ec49600c
Deepak <grc.deepak@gmail.com>
2016-09-28 19:14:48 -0700

    Digest Auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  http_basic_authenticate_with  :name => "foo",
-                                :password => "bar",
-                                :except => [:index, :show]
+  # http_basic_authenticate_with  :name => "foo",
+                                # :password => "bar",
+                                # :except => [:index, :show]
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
+  USERS = { "foo" => "bar" }
+  before_action :authenticate, :except => [:index, :show]
+
   # GET /users
   # GET /users.json
   def index
@@ -71,4 +74,11 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:username, :email)
     end
+
+    def authenticate
+      authenticate_or_request_with_http_digest do |username|
+        USERS[username]
+      end
+    end
+
 end
