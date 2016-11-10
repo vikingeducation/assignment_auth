ce9bfa4f2e944443d2c068da20b792ca5396ee4c
Tamal Riedman <triedman99@gmail.com>
2016-11-09 19:49:31 -0800

    Basic Auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :authenticate_user, :except => [:new, :edit, :create, :update, :destroy]
+
+  private
+
+  def authenticate_user
+    authenticate_or_request_with_http_basic('Message to User') do |username, password|
+      username == 'Foo' && password = 'bar'
+    end
+  end
+
 end
