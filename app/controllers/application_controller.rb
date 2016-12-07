daf345336eb0bb24338e414263096c93e7bc1115
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2016-12-07 12:29:17 -0500

    Convert HTTP Basic authentication to HTTP Digest authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate
+  CREDENTIALS = { "foo" => "bar" }
 
-  http_basic_authenticate_with name: "foo", password: "bar", except: [:index, :show]
+  private
+    def authenticate
+      authenticate_or_request_with_http_digest do |un|
+        CREDENTIALS[un]
+      end
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate, only: [:index, :show]
 
   # GET /users
   # GET /users.json
