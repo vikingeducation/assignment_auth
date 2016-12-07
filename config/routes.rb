942602fb546fc51533bb9bfa4fa7a78acf4c68dc
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2016-12-07 12:49:57 -0500

    Start session based authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/edit'
+
   root 'users#index'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+def generate_users(n)
+  n.times { create_user }
+end
+
+def create_user
+  user_name = Faker::StarWars.character
+  email = "#{ user_name }@star.wars".downcase.gsub(" ", "_")
+  User.create!(username: user_name,
+               email: email,
+               password: "dark_side")
+end
+
+generate_users(5)
+puts "Created #{ User.count } users"
