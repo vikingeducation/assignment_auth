a80fc5ac7afc801f0a0a6d3bbff1bfa8b3a14cd3
John Sawyer <sawyer@sawyermerchant.com>
2016-12-07 12:35:53 -0500

    Digest Auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  http_basic_authenticate_with :name => "blorg", :password => "grolb", :only => [:index, :show]
+  # http_basic_authenticate_with :name => "blorg", :password => "grolb", :only => [:index, :show]
 
-  before_action :call_cookie
+  # before_action :call_cookie
+  #
+  # def call_cookie
+  #   p cookies
+  #   p session
+  # end
 
-  def call_cookie 
-    p cookies
-    p session
-  end
+  USERS = { "glorb" => "glorb"}
+
+  before_action :authenticate
+
+  private
+
+    def authenticate
+      authenticate_or_request_with_http_digest do |username|
+        USERS[username]
+      end
+    end
+
 
 end
