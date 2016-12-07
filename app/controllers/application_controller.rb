5c7b75de5c66e8185b14adcfec73dc9ba7c4389d
Sampson Crowley <sampsonsprojects@gmail.com>
2016-12-07 10:22:41 -0700

    complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate, :except => [:index, :show]
   protect_from_forgery with: :exception
+  USERS = { 'user' => 'password' }
+
+  private
+    def authenticate
+      authenticate_or_request_with_http_digest do |username|
+        USERS[username]
+      end
+    end
 end
