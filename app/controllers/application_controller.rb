aacb71d1345265a97632368d91194f327aca05b3
luke-schleicher <luke.j.schleicher@gmail.com>
2016-12-07 11:31:15 -0600

    base64

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  http_basic_authenticate_with :name => "foo",
+                               :password => "bar",
+                               :except => [:index, :show]
+
+                               dsjfsdl
+
 end
