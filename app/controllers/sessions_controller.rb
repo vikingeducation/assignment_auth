3811692a937aea1edacf9e58cbd28c1c3bf0b956
James Harris <computersherpa@gmail.com>
2016-12-07 12:43:27 -0600

    Controller and form work

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,9 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by_username(params[:username])
     if @user && @user.authenticate(params[:password])
-      # sign_in(@user)
+      sign_in(@user)
       flash[:success] = "You have signed in!"
+      redirect_to @user
     else
       flash[:warning] = "Unable to log in."
       render :new
@@ -15,5 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    sign_out
+    redirect_to users_path
   end
 end
