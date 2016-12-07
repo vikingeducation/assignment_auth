ce9fbedd8c74b1a5536f1ccccf237eb3be176700
James Harris <computersherpa@gmail.com>
2016-12-07 11:11:31 -0600

    http_basic_authenticate_with

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with name: "name", password: "something", except: [:index, :show]
   protect_from_forgery with: :exception
 end
