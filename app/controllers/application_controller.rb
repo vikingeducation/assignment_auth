1b13b1bfe4ade14f6e0aed77ed13575a065a28e9
Pramod Jacob <psjacob68@gmail.com>
2016-12-07 12:28:31 -0500

    finished HTTP basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  http_basic_authenticate_with :name => "blorg", :password => "grolb", :only => [:index, :show]
+
+  before_action :call_cookie
+
+  def call_cookie 
+    p cookies
+    p session
+  end
+
 end
