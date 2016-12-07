567bc610e20a308813f2351d38bbd737982f3f24
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2016-12-07 12:49:40 -0500

    Remove HTTP Digest authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate
-  CREDENTIALS = { "foo" => "bar" }
-
-  private
-    def authenticate
-      authenticate_or_request_with_http_digest do |un|
-        CREDENTIALS[un]
-      end
-    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  skip_before_action :authenticate, only: [:index, :show]
 
   # GET /users
   # GET /users.json
