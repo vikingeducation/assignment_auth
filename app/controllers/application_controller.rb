f7a4aaf4bebfc7f8515627fc7ce5e9f473a8110d
Conor Fischer <conor.fischer@gmail.com>
2017-01-13 23:43:43 -0600

    Add HTTP Basic Auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  http_basic_authenticate_with name: "admin",
+                               password: "admin",
+                               only: [:update, :create]
 end
