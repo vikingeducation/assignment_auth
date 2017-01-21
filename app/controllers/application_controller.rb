70766cb36e1a192e92ee8ab45503d81271f9f163
tt <ttwang.303@gmail.com>
2017-01-20 22:18:24 -0500

    digest

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,21 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: 'foo',
-                               password: 'bar',
-                               except: [:index, :show]
+  # http_basic_authenticate_with name: 'foo',
+  #                              password: 'bar',
+  #                              except: [:index, :show]
   protect_from_forgery with: :exception
+
+  USERS = { "foo" => 'bar' }
+  before_action :authenticate, except: [:index, :show]
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_digest do |username|
+      USERS[username]
+    end
+  end
+
+
+
+
 end
