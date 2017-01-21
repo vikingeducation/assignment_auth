bcf59b49262509efb5bf2b19574fcdf53e60842f
tt <ttwang.303@gmail.com>
2017-01-20 22:07:57 -0500

    basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with name: 'foo',
+                               password: 'bar',
+                               except: [:index, :show]
   protect_from_forgery with: :exception
 end
