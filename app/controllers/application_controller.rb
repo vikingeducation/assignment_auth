726c16ee6771a9fbfa4c0961a6e00361bbb7e709
Behdad <behdad.analui@gmail.com>
2017-03-11 17:40:16 -0800

    added basic http authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+	http_basic_authenticate_with  :name => ENV["HTTP_BASIC_USER"], 
+                                  :password => ENV["HTTP_BASIC_PASS"],
+                                  :only => [:update, :create]
+  	protect_from_forgery with: :exception
 end
