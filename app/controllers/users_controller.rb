686f39e178cc58a0816b43ee577fd050d274b10d
Behdad <behdad.analui@gmail.com>
2017-03-12 10:54:31 -0700

    Added required Authorization and session based authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_current_user, :only => [:edit, :update, :destroy]
+
 
   # GET /users
   # GET /users.json
   def index
     @users = User.all
+    puts "\n\n\n\n\nSIGNED IN USER IS #{signed_in_user?}\n\n\n\n\n"
   end
 
   # GET /users/1
@@ -28,6 +31,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        sign_out
         sign_in(@user)
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
@@ -56,6 +60,7 @@ class UsersController < ApplicationController
   # DELETE /users/1.json
   def destroy
     @user.destroy
+    sign_out
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
