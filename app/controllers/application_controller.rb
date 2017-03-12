55a7ebbd63c5b7e2b4aa048dfdf1c2bc22bba56e
Behdad <behdad.analui@gmail.com>
2017-03-11 17:47:04 -0800

    added digest authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
-	http_basic_authenticate_with  :name => ENV["HTTP_BASIC_USER"], 
-                                  :password => ENV["HTTP_BASIC_PASS"],
-                                  :only => [:update, :create]
+
   	protect_from_forgery with: :exception
+
+ 	USERS = { ENV["HTTP_BASIC_USER"] => ENV["HTTP_BASIC_PASS"] }
+
+  	before_action :authenticate
+
+  	private
+
+    	def authenticate
+      		authenticate_or_request_with_http_digest do |username|
+        		USERS[username]
+      		end
+    	end
 end
