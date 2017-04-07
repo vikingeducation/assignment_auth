d85475e3d183c84c8c15da92cefb0a5be1a6a18c
Yi-Xuan Lau <yixuan.lau@gmail.com>
2017-04-07 13:37:50 +0800

    Implement HTTP Digest Auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  http_basic_authenticate_with( name: 'user', password: 'password', only: [:update, :create, :destroy])
+
+  USERS = { 'user' => 'password'}
+
+  # before_action :authenticate_basic, except: [:show, :index]
+  before_action :authenticate_digest, except: [:show, :index]
+
+
+  private
+
+  def authenticate_basic
+    authenticate_or_request_with_http_basic do |u, pass|
+      u == 'user' && pass == 'password'
+    end
+  end
+
+  def authenticate_digest
+    authenticate_or_request_with_http_digest do |username|
+      USERS[username]
+    end
+  end
 end
