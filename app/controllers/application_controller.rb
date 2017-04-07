3a56d16aa71b8ff6c167d9e1ece0ff2ca07b5900
Yi-Xuan Lau <yixuan.lau@gmail.com>
2017-04-07 10:53:39 +0800

    Implement HTTP Basic Auth for update, create, destroy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  http_basic_authenticate_with( name: 'user', password: 'password', only: [:update, :create, :destroy])
 end
