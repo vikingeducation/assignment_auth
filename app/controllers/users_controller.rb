7d0bfffb31416ad2ec3ba136fee3c5d835d8beed
efraimmgon <efraimmaugusto@gmail.com>
2017-06-05 16:34:38 -0400

    basic authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
+  http_basic_authenticate_with name: "foo",
+                               password: "foobar",
+                               only: [:create, :update]
+
   # GET /users
   # GET /users.json
   def index
