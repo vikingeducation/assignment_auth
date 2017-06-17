b177b286c377163d04aa646f5b070ca4417fca11
Visiona <darbis@gmail.com>
2017-06-17 19:08:50 +0100

    Set Basic Authorisation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
+  http_basic_authenticate_with  :name => "tester",
+                                :password => "owls15",
+                                :only => [:index, :show]
 end
