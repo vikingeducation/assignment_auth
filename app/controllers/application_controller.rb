f36a03ace83e42c9670206bbf58166c5ed9ddb6f
Visiona <darbis@gmail.com>
2017-06-17 19:18:34 +0100

    Replaced with Digest Authentication test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
-  http_basic_authenticate_with  :name => "tester",
-                                :password => "owls15",
-                                :only => [:index, :show]
+  # http_basic_authenticate_with  :name => "tester",
+  #                               :password => "owls15",
+  #                               :only => [:index, :show]
+
+  USERS = {"tester2" => "frogs"}
+
+  before_action :authenticate, :only => [:index, :show]
+
+  def authenticate
+    authenticate_or_request_with_http_digest do |username|
+      USERS[username]
+    end
+  end
 end
