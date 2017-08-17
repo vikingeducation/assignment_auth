5a0ac7a1918d377e667d326c5cb6710c08914bdb
Ann Allan <ann.cristina.allan@gmail.com>
2017-08-17 18:51:09 -0400

    Add Basic Authentication

--- basic/app/controllers/application_controller.rb
+++ basic/app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  http_basic_authenticate_with :name =>"carl",
+                              :password => "cup",
+                              :except => [:index, :show]
+
 end
