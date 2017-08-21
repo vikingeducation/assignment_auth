7a20e3bcbef33ac186166ad8ae29b731bedde4dd
Ann Allan <ann.cristina.allan@gmail.com>
2017-08-21 13:40:32 -0400

    Update gem file, bcryt, faker, hirb, better_errors, binding of caller, jazz fingers, will paginate

--- basic/db/seeds.rb
+++ basic/db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Destroying old records..."
+User.destroy_all
+puts "Old records destroyed."
+
+puts "Creating 5 Penguin Users..."
+User.create(username: "CarlHagelin",
+            email: "carl@pens.com",
+            password_digest: BCrypt::Password.create('carl62'))
+
+User.create(username: "PatricHornqvist",
+            email: "horny@pens.com",
+            password_digest: BCrypt::Password.create('patric72'))
+
+User.create(username: "SidneyCrosby",
+            email: "sid@pens.com",
+            password_digest: BCrypt::Password.create('sid87'))
+
+User.create(username: "ConorSheary",
+            email: "conor@pens.com",
+            password_digest: BCrypt::Password.create('conor43'))
+
+User.create(username: "EvgeniMalkin",
+            email: "geno@pens.com",
+            password_digest: BCrypt::Password.create('geno81'))
+puts "5 Pens created."
