9d43eb655efb487a965f0451e256b65810e45c86
Ann Allan <ann.cristina.allan@gmail.com>
2017-08-21 12:59:08 -0400

    Reset app

--- basic/app/controllers/application_controller.rb
+++ basic/app/controllers/application_controller.rb
@@ -1,13 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user
-  USERS = {"carl" => "cup"}
-
-  private
-  def authenticate_user
-    authenticate_or_request_with_http_digest do |username|
-      USERS[username]
-    end
-  end
-
 end
