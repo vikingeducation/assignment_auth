8cbf1af7c97bbca2d94ea20b49be68bf3662df6a
Ann Allan <ann.cristina.allan@gmail.com>
2017-08-21 13:37:21 -0400

    Add Digest Auth

--- basic/app/controllers/application_controller.rb
+++ basic/app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with :name => "Carl",
-                               :password => "carl62",
-                               :except => [:index, :show, :new, :create]
+  USERS = {"hags" => "carl62"}
   protect_from_forgery with: :exception
+  before_action :authenticate_user, :except => [:new, :index, :create]
+
+  private
+  def authenticate_user
+    authenticate_or_request_with_http_digest do |username|
+      USERS[username]
+    end
+  end
 end
