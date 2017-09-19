dd7606c57bb29e6e1fdacf9d2f9f0ceac4c95df8
David Wiesenberg <david_britta@yahoo.com>
2017-09-19 17:25:18 -0400

    practised HTTP basic authentication:

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  http_basic_authenticate_with  :name => "foo", 
+                                :password => "bar",
+                                :only => [:update, :create, :destroy]
+
 end
