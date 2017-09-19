9813c3fb32b5bd56b1ae63992e253eaa067e2488
David Wiesenberg <david_britta@yahoo.com>
2017-09-19 12:42:54 -0400

    added scaffold User

--- z_ann_allan_assignment_basic_auth-master/basic/config/routes.rb
+++ z_ann_allan_assignment_basic_auth-master/basic/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-
-  resources :users
   root 'users#index'
-
+  resources :users
+  resource  :session, :only=> [:new, :create, :destroy]
+  get "login" => 'sessions#new'
+  delete "logout" => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
