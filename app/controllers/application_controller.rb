1e61e08ac263219d6d7be8ae45d3e2864cab633a
Roy Chen <roychenlei@gmail.com>
2017-09-25 14:57:22 +0800

    added session authentication functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,36 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :require_login
+
+  def login(user)
+    session[:user_id] = user.id
+    @current_user = user
+  end
+  helper_method :login
+
+  def logout
+    @current_user = nil
+    session.delete(:user_id)
+  end
+  helper_method :logout
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  def logged_in_user?
+    !!current_user
+  end
+  helper_method :logged_in_user?
+
+  private
+
+  def require_login
+    unless logged_in_user?
+      flash[:error] = "You do not have permission to access this page. Please log in."
+      redirect_to login_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   resources :users
 
   resource :session, only: [:new, :create, :destroy]
-  get "login", to: "sessions#new"
-  get "logout", to: "sessions#destroy"
+  get "login", to: "session#new"
+  delete "logout", to: "session#destroy"
+
+  root to: "users#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
