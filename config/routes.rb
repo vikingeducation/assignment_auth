21dedcf41b2fef612c74ae98e221bd7c6b1c1b0b
Roy Chen <roychenlei@gmail.com>
2017-09-25 12:31:28 +0800

    added routes for session, created Session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :users
+
+  resource :session, only: [:new, :create, :destroy]
+  get "login", to: "sessions#new"
+  get "logout", to: "sessions#destroy"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
