b16a5d658441219177e4153655b2b7f0bab3a664
Roy Chen <roychenlei@gmail.com>
2017-09-25 12:15:44 +0800

    added password_digest column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170925021546) do
+ActiveRecord::Schema.define(version: 20170925041415) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
