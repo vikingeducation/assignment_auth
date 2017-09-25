87119c1ce84a970da7ea2c00fde179f67d39250e
Roy Chen <roychenlei@gmail.com>
2017-09-25 12:12:30 +0800

    created basic seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def destroy_all_data
+  puts "Destroying all data.."
+  User.destroy_all
+  puts "All data destroyed."
+end
+
+def create_users(num_users = 9)
+  puts "Creating random users.."
+  num_users.times do
+    username = Faker::Internet.user_name
+    User.create!(username: username, email: Faker::Internet.safe_email(username))
+  end
+  puts "Random users created."
+end
+
+def create_foobar_user
+  puts "Creating Mr Foo Bar.."
+  User.create!(username: "foobar", email: "foo@bar.com")
+  puts "Done."
+end
+
+destroy_all_data
+create_users
+create_foobar_user
