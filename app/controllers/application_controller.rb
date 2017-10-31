2d3759ee1384c117be2f2dbe11fe32ff3a798ac9
andie5 <andieasa@googlemail.com>
2017-10-31 23:29:42 +0000

    Digest authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,30 +1,31 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
-  before_action :authenticate_user, :except => [:new, :index]
-
-
-
-  USERS = { "foo" => "bar" }
-
-
+  # protect_from_forgery with: :exception
+  before_action :authenticate_user, :except => [:new, :index, :show ]
+  
+  # The HTTP Basic version (better)
   # http_basic_authenticate_with  :name => "foo",
   #                               :password => "bar",
   #                               :only => [:update, :create]
+
   private
-  # The HTTP Digest version (better)
+
+  # The HTTP Basic version
   # def authenticate_user
-  #   authenticate_or_request_with_http_digest do |username|
-  #     USERS[username]
+  #   authenticate_or_request_with_http_basic('Checking credentials') do |username, password|
+  #     username == "foo" && password == "bar"
   #   end
   # end
 
-  # The HTTP Basic version
+  USERS = { "user1" => "hellooooo", 
+            "user2" => "pwd2" }
+  
+  # The HTTP Digest version (better)
   def authenticate_user
-    authenticate_or_request_with_http_basic('Checking credentials') do |username, password|
-      username == "foo" && password == "bar"
+    authenticate_or_request_with_http_digest do |username|
+      USERS[username]
     end
   end
+
 end
 
 
