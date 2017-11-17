93347821fa27b0fd68df62b4627955bb53d6e8cc
Sean Luckett <whitecrane007@gmail.com>
2017-11-17 14:48:17 -0700

    Sets up session auth paths and seeds db with some users

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,9 @@ ActiveRecord::Schema.define(version: 20171115184945) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do |i|
+  User.create name: "Pat #{i}",
+              email: "pat#{i}.just@nyc.com",
+              password: 'foobar',
+              password_confirmation: 'foobar'
+end
