778186c36b2546e561147ff4131fe69f1d74f0cc
anne <richardson.ae@gmail.com>
2018-01-03 09:47:01 -0600

    Implements basic http authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  http_basic_authenticate_with  :name => "anne",
+                                  :password => "password",
+                                  :except => [:index, :show]
 end
